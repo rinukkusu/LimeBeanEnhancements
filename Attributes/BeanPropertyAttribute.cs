ffe2b70f88743ef6d21f6c59b81e4323b89f00d3
Maximilian Riegler <mriegler@Lyoness.local>
2016-08-10 18:05:12 +0200

    initial stuff

---
+++ Attributes/BeanPropertyAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LimeBeanEnhancements.Attributes
+{
+	[AttributeUsage(AttributeTargets.Property)]
+    public class BeanPropertyAttribute : Attribute
+    {
+		public string Column { get; private set; }
+		public int Position { get; private set; }
+		public bool SaveToDatabase { get; private set; }
+
+		public BeanPropertyAttribute(string column, int position = 1, bool saveToDatabase = true)
+	    {
+			Column = column;
+			Position = position;
+			SaveToDatabase = saveToDatabase;
+	    }
+    }
+}
---
+++ Attributes/BeanRelationAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LimeBeanEnhancements.Attributes
+{
+	[AttributeUsage(AttributeTargets.Property)]
+	public class BeanRelationAttribute : Attribute
+    {
+		public Type RelatedBeanType { get; private set; }
+
+		public BeanRelationAttribute(Type relatedBeanType)
+		{
+			RelatedBeanType = relatedBeanType;
+		}
+	}
+}
